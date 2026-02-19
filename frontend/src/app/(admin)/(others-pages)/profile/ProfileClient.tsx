c29ab4dcddd51910e80d30c722275c8654f75142
Anubhav <frontend@skinelements.com>
2026-02-19 17:30:52 +0530

    updated

--- frontend/src/app/(admin)/(others-pages)/profile/ProfileClient.tsx
+++ frontend/src/app/(admin)/(others-pages)/profile/ProfileClient.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import UserInfoCard from "@/components/user-profile/UserInfoCard";
 import UserAddressCard from "@/components/user-profile/UserAddressCard";
 import Button from "@/components/ui/button/Button";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 
 function TabButton({
   active,
@@ -30,6 +31,24 @@ export default function ProfileClient() {
   return (
     <div>
       <div className="rounded-2xl">
+        {/* <div className="sticky top-0 z-40 w-full flex flex-col bg-white  md:flex-row md:items-center md:justify-between gap-4 border-b border-gray-200"> */}
+
+          {/* LEFT: Title + Subtitle */}
+          <div className="flex flex-col leading-tight w-full md:w-auto md:mb-5">
+            <PageBreadcrumb
+              pageTitle="Account Settings"
+              variant="page"
+              align="left"
+              textSize="2xl"
+            />
+
+
+
+            <p className="text-xs 2xl:text-sm text-charcoal-500 mt-1">
+              Manage your profile, country, integrations and performance targets
+            </p>
+          </div>
+        {/* </div> */}
         {/* Tabs */}
         <div className="flex flex-wrap gap-2 ">
           <TabButton
